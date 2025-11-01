747ca22bc4f95fc20e51e3aa7b0e1081c84039ad
Prem Patel <88373796+prempatel14020@users.noreply.github.com>
2025-10-31 21:41:13 -0600

    Worked on footer, menu, contact, catering  lo-fi WordPress templates and their CSS

--- functions.php
+++ functions.php
@@ -1,4 +1,46 @@
 <?php
 add_action( 'wp_enqueue_scripts', function() {
     wp_enqueue_style( 'deli-theme-style', get_stylesheet_uri() );
+
+
+    // Page-specific styles
+
+    if ( is_front_page(  ) ) {
+        wp_enqueue_style(
+            'front-page-style',
+            get_template_directory_uri() . '/assets/css/front-page.css'
+        );
+    }
+
+    // About Page
+     if ( is_page( 'about' ) ) {
+        wp_enqueue_style(
+            'about-style',
+            get_template_directory_uri() . '/assets/css/about.css'
+        );
+    }
+
+    // menu page
+    if ( is_page( 'menu' ) ) {
+        wp_enqueue_style(
+            'menu-style',
+            get_template_directory_uri() . '/assets/css/menu.css'
+        );
+    }
+
+    // Catering page
+      if ( is_page( 'catering' ) ) {
+        wp_enqueue_style(
+            'catering-style',
+            get_template_directory_uri() . '/assets/css/catering.css'
+        );
+    }
+
+    // contact page
+      if ( is_page( 'contact' ) ) {
+        wp_enqueue_style(
+            'contact-style',
+            get_template_directory_uri() . '/assets/css/contact.css'
+        );
+    }
 });
